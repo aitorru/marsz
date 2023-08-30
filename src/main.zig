7ad09cf011bdd57f5684abd7f85565e5e4a36e73
aitorru <aitorruiz2m@gmail.com>
2023-08-30 14:37:01 +0200

    Added zap to the dependencies

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,33 @@
 const std = @import("std");
+const zap = @import("zap");
 
+// TODO: It does not work.
 extern fn start(c: bool) bool;
 
 pub fn main() !void {
-    std.debug.print("Hello from zig\n", .{});
-    const result = start(false);
-    std.debug.print("Result from rust: {any}\n", .{result});
+    var listener = zap.SimpleHttpListener.init(.{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+
+    // start worker threads
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
+}
+
+fn on_request(r: zap.SimpleRequest) void {
+    if (r.path) |the_path| {
+        std.debug.print("PATH: {s}\n", .{the_path});
+    }
+
+    if (r.query) |the_query| {
+        std.debug.print("QUERY: {s}\n", .{the_query});
+    }
+    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
 }
