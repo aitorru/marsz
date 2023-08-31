21855d5b0cdbc545a813678c4f84c42b0e9829d8
aitorru <aitorruiz2m@gmail.com>
2023-08-31 17:23:35 +0200

    Added devcontainer for coding in windows

--- src/main.zig
+++ src/main.zig
@@ -4,30 +4,59 @@ const zap = @import("zap");
 // TODO: It does not work.
 extern fn start(c: bool) bool;
 
+fn dispatch_routes(r: zap.SimpleRequest) void {
+    // dispatch
+    if (r.path) |the_path| {
+        if (routes.get(the_path)) |handler| {
+            handler(r);
+            return;
+        }
+    }
+    // or default: present menu
+    r.sendBody(@embedFile("../deimos/_site/index.html")) catch return;
+}
+fn return_index(r: zap.SimpleRequest) void {
+    r.sendBody(@embedFile("../deimos/_site/index.html"));
+}
+
+fn static_site(r: zap.SimpleRequest) void {
+    r.sendBody("<html><body><h1>Hello from STATIC ZAP!</h1></body></html>") catch return;
+}
+
+var dynamic_counter: i32 = 0;
+fn dynamic_site(r: zap.SimpleRequest) void {
+    dynamic_counter += 1;
+    var buf: [128]u8 = undefined;
+    const filled_buf = std.fmt.bufPrintZ(
+        &buf,
+        "<html><body><h1>Hello # {d} from DYNAMIC ZAP!!!</h1></body></html>",
+        .{dynamic_counter},
+    ) catch "ERROR";
+    r.sendBody(filled_buf) catch return;
+}
+
+fn setup_routes(a: std.mem.Allocator) !void {
+    routes = std.StringHashMap(zap.SimpleHttpRequestFn).init(a);
+    try routes.put("/", return_index);
+    try routes.put("/static", static_site);
+    try routes.put("/dynamic", dynamic_site);
+}
+
+var routes: std.StringHashMap(zap.SimpleHttpRequestFn) = undefined;
+
 pub fn main() !void {
+    try setup_routes(std.heap.page_allocator);
     var listener = zap.SimpleHttpListener.init(.{
         .port = 3000,
-        .on_request = on_request,
+        .on_request = dispatch_routes,
         .log = true,
     });
     try listener.listen();
 
     std.debug.print("Listening on 0.0.0.0:3000\n", .{});
 
-    // start worker threads
     zap.start(.{
         .threads = 2,
         .workers = 2,
     });
-}
-
-fn on_request(r: zap.SimpleRequest) void {
-    if (r.path) |the_path| {
-        std.debug.print("PATH: {s}\n", .{the_path});
-    }
-
-    if (r.query) |the_query| {
-        std.debug.print("QUERY: {s}\n", .{the_query});
-    }
-    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
 }
