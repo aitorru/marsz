4886a394d3dd8e394615c5e67e5627cadcb1fd48
aitorru <aitorruiz2m@gmail.com>
2023-09-01 01:40:17 +0200

    holy fuck rust is working

--- phobos/src/lib.rs
+++ phobos/src/lib.rs
@@ -1,5 +1,10 @@
 #[no_mangle]
 pub extern "C" fn start(c: bool) -> bool {
-    // println!("Hello from Rust!");
+    println!("Hello from Rust!");
     c
 }
+
+// #[no_mangle]
+// pub extern "C" fn formatString<'a>(c: &'a [u8], args: &'a [u8]) -> &'a [u8] {
+//     c
+// }
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+const fs = std.fs;
 const zap = @import("zap");
 
 // TODO: It does not work.
 extern fn start(c: bool) bool;
+// extern fn formatString(c: []u8, args: []u8) []u8;
 
 fn dispatch_routes(r: zap.SimpleRequest) void {
     // dispatch
@@ -13,10 +15,34 @@ fn dispatch_routes(r: zap.SimpleRequest) void {
         }
     }
     // or default: present menu
-    r.sendBody(@embedFile("../deimos/_site/index.html")) catch return;
+    r.sendBody("a") catch return;
 }
+var index_contents: []u8 = undefined;
 fn return_index(r: zap.SimpleRequest) void {
-    r.sendBody(@embedFile("../deimos/_site/index.html"));
+    if (index_contents.len == 0) {
+        // Open the index file
+        var file = std.fs.cwd().openFile("deimos/_site/index.html", .{}) catch return;
+        defer file.close();
+
+        var buf_reader = std.io.bufferedReader(file.reader());
+        var in_stream = buf_reader.reader();
+
+        // Reader buffer
+        var buf: [1024]u8 = undefined;
+
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        var allocator = arena.allocator();
+
+        while (in_stream.readUntilDelimiterOrEof(&buf, '\n') catch return) |line| {
+            // Concatenate the contents of the file to the local variable index_contents
+
+            index_contents = std.fmt.allocPrint(allocator, "{s}{s}", .{ if (index_contents.len == 0) "" else index_contents, line }) catch return;
+        }
+    }
+    // std.debug.print("{s}", .{index_contents});
+    std.debug.print("Is rust working?: {}\n", .{!start(false)});
+    r.sendBody("zzz") catch return;
 }
 
 fn static_site(r: zap.SimpleRequest) void {
